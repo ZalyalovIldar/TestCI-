1a9b14fc2a0328403e33e22ff8b033511366a793
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-08-31 12:00:40 +0300

    Initial Commit

---
+++ TestProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Ильдар Залялов on 31/08/2019.
+//  Copyright © 2019 Ильдар Залялов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
